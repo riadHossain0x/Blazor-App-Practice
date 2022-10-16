fa8d4c1cfea3a8d4ccb70d3aca51aaf9d1fe0fa3
Riad <riadofficial.me@gmail.com>
2022-10-16 15:24:36 +0600

    practice

---
+++ Templated Components - Generic Typed RenderFragment/Entities/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace Templated_Components___Generic_Typed_RenderFragment.Entities
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Address { get; set; } = null!;
+    }
+}
