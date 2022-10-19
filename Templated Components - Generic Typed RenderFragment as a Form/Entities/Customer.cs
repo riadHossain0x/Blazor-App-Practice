9b5319120f1498e71c8e82de2d1d90c8ff83269b
Riad <riadofficial.me@gmail.com>
2022-10-19 10:25:01 +0600

    practice

---
+++ Templated Components - Generic Typed RenderFragment as a Form/Entities/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace Templated_Components___Generic_Typed_RenderFragment_as_a_Form.Entities
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+    }
+}
